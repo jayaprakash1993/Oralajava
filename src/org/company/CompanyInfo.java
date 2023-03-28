4de2b98e9449833d0f9d3b56559a866c407671bb
jayaprakash1993 <jpksastra@gmail.com>
2023-03-28 17:33:30 +0530

    Done

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,34 @@
+package org.company;
+
+public class CompanyInfo {
+
+	private void companyName() {
+		System.out.println("Company Name is Greens Technologies");
+	}
+	
+	private void companyId() {
+		System.out.println("Company Id is 1234");
+	}
+	
+	private void companyAddress() {
+	System.out.println("Company Address is Tambaram chennai");
+	}
+	
+	public static void main(String[] args) {
+		CompanyInfo ad = new CompanyInfo();
+		
+		ad.companyName();
+		ad.companyId();
+		ad.companyAddress();
+				
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
